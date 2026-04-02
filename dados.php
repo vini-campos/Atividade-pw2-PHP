e6df7a0bc59a12df77c4fde4bc6ce80c07eed0a2
Vinicius Campos <viniciuscampos131209@gmail.com>
2026-04-02 20:25:44 -0300

    página de dados

--- dados.php
+++ dados.php
@@ -1,1 +1,25 @@
-//fazer os dados do form do index.php aparecerem aqui.
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Formulário</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="form-container">
+        <h1>Dados do Formulário</h1>
+        <p><strong>Nome:</strong> <?php echo $_POST['nome']; ?></p>
+        <p><strong>Sobrenome:</strong> <?php echo $_POST['sobrenome']; ?></p>
+        <p><strong>Email:</strong> <?php echo $_POST['email']; ?></p>
+        <p><strong>Endereço:</strong> <?php echo $_POST['endereco']; ?></p>
+        <p><strong>Telefone:</strong> <?php echo $_POST['telefone']; ?></p>
+        <p><strong>Data de Nascimento:</strong> <?php echo $_POST['datanasc']; ?></p>
+        //colocar o campo de aparecer a foto aqui.....
+    </div>
+</body>
+
+</html>
