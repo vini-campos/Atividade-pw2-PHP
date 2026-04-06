d1fb8453ebea7036a34a50fbbef9a8549afa543d
Vinicius Campos <viniciuscampos131209@gmail.com>
2026-04-06 18:26:38 -0300

    ajustes na responsividade, formulário agora funcionando com css-grid.

--- dados.php
+++ dados.php
@@ -10,42 +10,39 @@
 </head>
 
 <body>
-
-    
-
-    <div class="form-container">
-        <h1>Dados do Formulário</h1>
-        <p><strong>Nome:</strong> <?php echo $_POST['nome']; ?></p>
-        <p><strong>Sobrenome:</strong> <?php echo $_POST['sobrenome']; ?></p>
-        <p><strong>Email:</strong> <?php echo $_POST['email']; ?></p>
-        <p><strong>Endereço:</strong> <?php echo $_POST['endereco']; ?></p>
-        <?php
-            $telefone = $_POST['telefone'];
-            $a = substr($telefone,  0, 2);
-        ?>
-        <p><strong>Telefone:</strong> <?php echo $telefone?></p>
-        <?php
-            $a = $_POST["datanasc"];
-            $d = new DateTime("$a", new DateTimeZone("America/Sao_Paulo"));
-        ?>
-        <p><strong>Data de Nascimento:</strong> <?php echo $d->format("d/m/Y")?></p>
-        
-        <div>
-            <?php
-
-            if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['arq'])) {
-
-                $nome = $_FILES['arq']['name'];
-                $tmp = $_FILES['arq']['tmp_name'];
-
-                $caminho = "uploads/" . $nome;
-
-                move_uploaded_file($tmp, $caminho);
-            } ?>
-            <div style="align-items: center; display: flex; flex-direction: column;">
-                <?php echo "<img src='$caminho' width='500' height=auto>"; ?>
+    <div class="form-grid">
+        <div class="form-esquerda">
+            <div class="form-container">
+                <h1>Dados do Formulário</h1>
+                <p><strong>Nome:</strong> <?php echo $_POST['nome']; ?></p>
+                <p><strong>Sobrenome:</strong> <?php echo $_POST['sobrenome']; ?></p>
+                <p><strong>Email:</strong> <?php echo $_POST['email']; ?></p>
+                <p><strong>Endereço:</strong> <?php echo $_POST['endereco']; ?></p>
+                <?php
+                    $telefone = $_POST['telefone'];
+                    $a = substr($telefone,  0, 2);
+                ?>
+                <p><strong>Telefone:</strong> <?php echo $telefone?></p>
+                <?php
+                    $a = $_POST["datanasc"];
+                    $d = new DateTime("$a", new DateTimeZone("America/Sao_Paulo"));
+                ?>
+                <p><strong>Data de Nascimento:</strong> <?php echo $d->format("d/m/Y")?></p>
             </div>
+        </div>
 
+        <div class="form-direita">
+            <?php
+                if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_FILES['arq'])) {
+                    $nome = $_FILES['arq']['name'];
+                    $tmp = $_FILES['arq']['tmp_name'];
+                    $caminho = "uploads/" . $nome;
+                    move_uploaded_file($tmp, $caminho);
+                }
+            ?>
+            <div style="align-items: center; justify-content: center; display: flex;">
+                <?php echo "<img src='$caminho' style='max-width: 100%; height: auto; max-height: 300px;'>"; ?>
+            </div>
         </div>
     </div>
 </body>
